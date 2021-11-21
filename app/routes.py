38db6783cba068d0afa68e8755152a2a94af0b1b
Akash <akasharya1304@gmail.com>
2021-11-21 22:29:24 +0530

    chapter - 2   Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World!"
+    user = {"username" : "Akash Kumar"}
+    posts = [
+        {
+            "author" : {"username" : "John"},
+            "body" : "Today is very cold day"
+        },
+        {
+            "author" : {"username" : "Henry"},
+            "body" : "Avenger movie is so cool"
+        }
+            ]
+    return render_template("index.html", title="Home" , user=user , posts=posts)
